90f4aead68248cf8fb1c85e7c337a553e554ff6c
youjing <youjing@gmail.com>
2023-10-27 13:01:15 +0800

    go Iterator pattern

---
+++ book-learn/pC-Iterator-pattern/generic/main.go
@@ -0,0 +1,27 @@
+package main
+
+/**
+使用go泛型实现迭代器模式
+*/
+
+//// Iterator 迭代器
+//type Iterator[T any] interface {
+//	HashNext() bool
+//	Next() T
+//}
+//
+//type MyIterator[T any] struct {
+//	data  []*T
+//	index int
+//}
+//
+//func NewMyIterator[T any](data []*T) *MyIterator {
+//	return &MyIterator{
+//		data:  data,
+//		index: 0,
+//	}
+//}
+//
+//func (my *MyIterator) HashNext() bool {
+//	return my.index < len(my.data)
+//}
